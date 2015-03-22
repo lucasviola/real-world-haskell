b54fe46261c357ff50fb385cb0eabf4bbf336b4d
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-22 09:22:43 -0300

    *

--- myDrop.hs
+++ myDrop.hs
@@ -1,6 +1,8 @@
+myDrop :: Int -> [a] -> [a]
 myDrop n xs = if n <= 0 || null xs
 			  then xs
 			  else myDrop (n - 1) (tail xs)
 
 -- Reescrevendo a função em uma linha
+myDrop' :: Int -> [a] -> [a]
 myDrop' n xs = if n <= 0 || null xs then xs else myDrop' (n - 1) (tail xs)
