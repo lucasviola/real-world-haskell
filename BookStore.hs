e05df8e98b1157ff236d87a7b1b3370536e5aece
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-22 09:33:50 -0300

    Adicionados Type Synonyms

--- BookStore.hs
+++ BookStore.hs
@@ -5,4 +5,18 @@ data BookInfo = Book Int String [String]
 data MagazineInfo = Magazine Int String [String]
 					deriving (Show, Eq)
 
+-- Aqui o Type Constructor e o Value Constructor tem o mesmo nome.
+-- Isso não tem problema porque eles são independentes, o TC é o tipo,
+-- vai ser usado na assinatura da função, e o VC é a função e vai ser usado
+-- no código.
+data BookReview = BookReview BookInfo CustomerID String
+				  deriving (Show)
+
+-- Type Synonyms: Usados para descrever melhor um determinado Tipo.
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+-- Construindo dados
 novoLivro = Book 1 "Haskell for dummies" ["Lucas", "Mel"]
