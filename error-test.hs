8678c4b33086ab4ce60b7c4731ed3e4b5d50abea
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-25 02:52:21 -0300

    criada função snd safer

--- error-test.hs
+++ error-test.hs
@@ -1,6 +1,15 @@
--- Caso a lista seja um singleton, gera um erro com a mensagem definida
+-- Caso a lista seja um singleton, gera um erro e aborta a execução
+-- Essa função não é safe
 
 snd' :: [a] -> a
 snd' xs = if null (tail xs)
 		 then error "a lista é muito pequena"
 		 else head (tail xs)
+
+-- Usando Maybe Monad
+-- Essa é safe
+sndSafe :: [a] -> Maybe a
+sndSafe [] = Nothing
+sndSafe xs = if null (tail xs)
+				then Nothing
+				else Just (head (tail xs))
